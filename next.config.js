967a6c205e19c284d2aca6b892dc702fa6c6508e
Loth <u19217724@gmail.com>
2023-11-08 18:34:49 -0500

    primer commit

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Otras configuraciones
+  
+}
 
-  webpack(config) {
-    config.resolve.alias['@'] = __dirname;
-    return config;
-  },
-};
-
-module.exports = nextConfig;
+module.exports = nextConfig
